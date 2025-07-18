81e8c5634ce4d2e3073957686496685b6316b8b7
Jedremi <69273603+Jedremi@users.noreply.github.com>
2025-07-18 09:56:23 -0300

    Add .next/static/chunks/src_app_admin_usuarios_page_tsx_0832bc84._.js 1752843383741

---
+++ .next/static/chunks/src_app_admin_usuarios_page_tsx_0832bc84._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_7a4bbdb1._.js",
+  "static/chunks/node_modules_7b07bd58._.js"
+],
+    source: "dynamic"
+});
